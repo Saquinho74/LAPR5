d072f7466f2678d1d795739c9d03106c730bcbfe
Francisco Silveira <1220813@isep.ipp.pt>
2024-10-21 17:38:52 +0100

    #3 add repository, controller, dto, mapper and service

---
+++ DTO/PatientDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using DDDNetCore.Domain.Families;
+
+namespace DDDNetCore.DTO;
+
+public class PatientDTO(PatientID entityPatientId, DateOfBirth entityDateOfBirth, Gender entityGender, MedicalRecordNumber entityMedicalRecordNumber, ContactInformation entityContactInformation, EmergencyContact entityEmergencyContact, AppointmentHistory entityAppointmentHistory, AllergiesMedicalConditions entityAllergiesMedicalConditions)
+{
+    public PatientID patientID { get; set; }
+    public DateOfBirth DateOfBirth { get; private set; }
+    public Gender Gender { get; private set; }
+    public MedicalRecordNumber MedicalRecordNumber { get; private set; }
+    public ContactInformation ContactInformation { get; private set; }
+    public EmergencyContact EmergencyContact { get; private set; }
+    public AppointmentHistory AppointmentHistory { get; private set; }
+    public AllergiesMedicalConditions AllergiesMedicalConditions { get; private set; }
+    
+}
---
+++ Infraestructure/Patient/PatientRepository.cs
@@ -0,0 +1,12 @@
+﻿using DDDNetCore.Domain.Families;
+using DDDNetCore.Infraestructure.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace DDDNetCore.Infraestructure.Patient;
+
+public class PatientRepository : BaseRepository<DDDSample1.Domain.Patients.Patient, PatientID>, IPatientRepository
+{
+    public PatientRepository(DbSet<DDDSample1.Domain.Patients.Patient> objs) : base(objs)
+    {
+    }
+}
