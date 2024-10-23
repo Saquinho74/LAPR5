d1d96bfadf78151abf42f9a518e0e80b3414b076
Marco <1220913@isep.ipp.pt>
2024-10-23 19:42:39 +0100

    StaffEntityTypeConfiguration

---
+++ Infraestructure/Staff/StaffEntityTypeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DDDNetCore.Infraestructure.Staff;
+
+public class StaffEntityTypeConfiguration : IEntityTypeConfiguration<Domain.Staff.Staff>
+{
+    public void Configure(EntityTypeBuilder<Domain.Staff.Staff> builder)
+    {
+        builder.ToTable("Staff", SchemaNames.DDDSample1);
+        builder.HasKey(b => b.Id);
+
+        builder.OwnsOne(b => b.Specialization,
+            nameBuilder => { nameBuilder.Property(p => p.Value).HasColumnName("Specialization"); });
+
+        builder.OwnsOne(s => s.LicenseNumber, ln =>
+        {
+            ln.Property(l => l.Value).HasColumnName("LicenseNumber"); // Ensure proper mapping
+        });
+
+        builder.HasMany(b => b.Slot)
+            .WithOne()
+            .HasForeignKey("Id")
+            .IsRequired();
+    }
+}
