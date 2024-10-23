b18caaeb5e5819c2e2a337cc85d8260e27935eb7
Francisco Silveira <1220813@isep.ipp.pt>
2024-10-23 19:36:38 +0100

    #9 add methods to edit atributes of the Patient

--- Domain/Patient/Patient.cs
+++ Domain/Patient/Patient.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using DDDNetCore.Domain.Families;
 using DDDNetCore.Domain.Patient;
 using DDDNetCore.Domain.Shared;
@@ -17,16 +16,20 @@ namespace DDDSample1.Domain.Patients
         public EmergencyContact EmergencyContact { get; private set; }
         public AppointmentHistory AppointmentHistory { get; private set; }
         public AllergiesMedicalConditions AllergiesMedicalConditions { get; private set; }
-        
-        public Patient( ) {}
+        public bool Active { get; private set; }
+
+        public Patient()
+        {
+            this.Active = true;
+        }
 
         // Constructor
-        public Patient(PatientId patientID, DateOfBirth dateOfBirth, Gender gender, 
-                        MedicalRecordNumber medicalRecordNumber, ContactInformation contactInformation,
-                        EmergencyContact emergencyContact, AppointmentHistory appointmentHistory,
-                        AllergiesMedicalConditions allergiesMedicalConditions)
+        public Patient(PatientId patientId, DateOfBirth dateOfBirth, Gender gender,
+            MedicalRecordNumber medicalRecordNumber, ContactInformation contactInformation,
+            EmergencyContact emergencyContact, AppointmentHistory appointmentHistory,
+            AllergiesMedicalConditions allergiesMedicalConditions)
         {
-            Id = patientID;
+            Id = patientId;
             DateOfBirth = dateOfBirth ?? throw new ArgumentNullException(nameof(dateOfBirth));
             Gender = gender;
             MedicalRecordNumber = medicalRecordNumber ?? throw new ArgumentNullException(nameof(medicalRecordNumber));
@@ -35,11 +38,75 @@ namespace DDDSample1.Domain.Patients
             AppointmentHistory = appointmentHistory ?? throw new ArgumentNullException(nameof(appointmentHistory));
             AllergiesMedicalConditions = allergiesMedicalConditions;
         }
-    }
-    
-   
 
-    // Value Objects and Identity
-    
-    }   
+        public void ChangeDateOfBirth(DateOfBirth dateOfBirth)
+        {
+            if (!this.Active)
+                throw new BusinessRuleValidationException("It is not possible to change the date of birth");
+            this.DateOfBirth = dateOfBirth;
+            {
+
+            }
+        }
+
+        public void ChangeGender(Gender gender)
+        {
+            if (!this.Active)
+                throw new BusinessRuleValidationException("It is not possible to change the date of birth");
+            this.Gender = gender;
+            {
+            }
+        }
+
+
+
+        // Value Objects and Identity
+
+        public void ChangeMedicalRecordNumber(MedicalRecordNumber medicalRecordNumber)
+        {
+            if (!this.Active)
+                throw new BusinessRuleValidationException("It is not possible to change the medical record number");
+            this.MedicalRecordNumber = medicalRecordNumber;
+            {
+            }
+        }
+
+        public void ChangeContactInformation(ContactInformation contactInformation)
+        {
+            if (!this.Active)
+                throw new BusinessRuleValidationException("It is not possible to change the contact information");
+            this.ContactInformation = contactInformation;
+            {
+                
+            }
+        }
+
+        public void ChangeEmergencyContact(EmergencyContact emergencyContact)
+        {
+            if (!this.Active)
+                throw new BusinessRuleValidationException("It is not possible to change the emergency contact");
+            this.EmergencyContact = emergencyContact;
+            {
+                
+            }
+        }
+
+        public void ChangeAppointmentHistory(AppointmentHistory appointmentHistory)
+        {
+            if (!this.Active)
+                throw new BusinessRuleValidationException("It is not possible to change the appointment history");
+            this.AppointmentHistory = appointmentHistory;
+            {
+                
+            }
+        }
+
+        public void ChangeAllergiesMedicalConditions(AllergiesMedicalConditions allergiesMedicalConditions)
+        {
+            if(!this.Active)
+                throw new BusinessRuleValidationException("It is not possible to change the allergies and medical conditions");
+            this.AllergiesMedicalConditions = AllergiesMedicalConditions;
+        }
+    }
+}
 
