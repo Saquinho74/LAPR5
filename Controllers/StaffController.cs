ecc86c927f629cbc4cd7e566281f6c5b2473ea16
Marco <1220913@isep.ipp.pt>
2024-10-23 19:41:54 +0100

    Staff Controller

---
+++ Controllers/StaffController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DDDNetCore.Domain.Staff;
+using DDDNetCore.DTO.StaffDto;
+using DDDNetCore.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DDDNetCore.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class StaffController : ControllerBase
+{
+    private readonly IStaffService _service;
+    
+    
+    public StaffController(IStaffService service)
+    {
+        _service = service;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<StaffDto>>> GetAll()
+    {
+            var staff = await _service.GetAllAsync();
+            return Ok(staff);
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<StaffDto>> GetById(string id)
+    {
+        var staff = await _service.GetByIdAsync(StaffId.Create(id));
+        if (staff == null)
+        {  
+            return NotFound();
+        }
+        return staff;
+    }
+    [HttpPost]
+    public async Task<ActionResult<StaffDto>> Create(CreatingStaffDto dto)
+    {
+        var staff = await _service.AddAsync(dto);
+        return CreatedAtAction(nameof(GetById), new { id = staff.StaffID }, staff);    
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<ActionResult<StaffDto>> Update([FromRoute] StaffId id, [FromBody] StaffDto dto)
+    {
+        if (id.ToString() != dto.StaffID)
+        {
+            return BadRequest();
+        }
+        try
+        {
+            var cat = await _service.UpdateAsync(dto);
+            if (cat == null)
+            {
+                return NotFound();
+            }
+            return Ok(cat);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new {Message = ex.Message});
+        }
+    }
+
+    [HttpDelete("{id}/hard")]
+    public async Task<ActionResult<StaffDto>> HardDelete(string id)
+    {
+        try
+        {
+            var cat = await _service.DeleteAsync(id);
+            if (cat == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cat);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { Message = ex.Message });
+        }
+    }
+
+
+
+}
