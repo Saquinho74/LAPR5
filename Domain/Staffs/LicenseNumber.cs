8ea01f277417bd8e5572806160b44274861a8274
Marco <1220913@isep.ipp.pt>
2024-10-23 19:42:11 +0100

    License Number rework from staff agregate

---
+++ Domain/Staffs/LicenseNumber.cs
@@ -0,0 +1,14 @@
+﻿using DDDNetCore.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace DDDNetCore.Domain.Staff;
+
+public class LicenseNumber : IValueObject
+{
+    public string Value { get; private set; }
+
+    public LicenseNumber(string value)
+    {
+        Value = value;
+    }
+}
