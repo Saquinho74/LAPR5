35f27952f257e91696df5953929be88df98b81a5
Marco <1220913@isep.ipp.pt>
2024-10-24 10:45:50 +0100

    #14 US 5.1.14 - fully implemented

--- DTO/StaffDto/StaffDto.cs
+++ DTO/StaffDto/StaffDto.cs
@@ -7,7 +7,10 @@ public class StaffDto
     public string StaffID { get; set; }
     public string LicenseNumber { get; set; }
     public string Specialization { get; set; }
-    public List<string> Slots { get; set; }  
-    
-   
+    public List<string> Slots { get; set; }
+
+    public string ToString()
+    {
+        return StaffID + " " + LicenseNumber + " " + Specialization + " " + Slots;
+    }
 }
